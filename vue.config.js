2d91490fdfc7d8534318174b0360145cd0e973c3
xiongzihaoc <xiongzihao1997@gmail.com>
2020-06-18 17:10:20 +0800

    打包压缩配置

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,41 @@
 module.exports = {
   publicPath: "./",
-  outputDir: "dist"
+  outputDir: "dist",
+  // 去除.map文件
+  productionSourceMap: false,
+  configureWebpack: () => ({
+    optimization: {
+      splitChunks: {
+        cacheGroups: {
+          vendor: {
+            chunks: "all",
+            test: /node_modules/,
+            name: "vendor",
+            minChunks: 1,
+            maxInitialRequests: 5,
+            minSize: 0,
+            priority: 100,
+          },
+          common: {
+            chunks: "all",
+            test: /[\\/]src[\\/]js[\\/]/,
+            name: "common",
+            minChunks: 2,
+            maxInitialRequests: 5,
+            minSize: 0,
+            priority: 60,
+          },
+          styles: {
+            name: "styles",
+            test: /\.(sa|sc|c)ss$/,
+            chunks: "all",
+            enforce: true,
+          },
+          runtimeChunk: {
+            name: "manifest"
+          },
+        }
+      }
+    }
+  }),
 }
